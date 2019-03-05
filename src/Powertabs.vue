5c07504a3ea4ad6ad3d3111c7b72149123537994
Georgi Georgiev <georgiev.georgi93@gmail.com>
2019-03-05 15:00:47 +0100

    working dev setup

---
+++ src/Powertabs.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>{{name}}</h1>
+        <button>Test</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+          return {
+              name: 'Powertabs'
+          }
+        },
+        created() {
+            browser.tabs.query({currentWindow: true}).then(this.logTabs, () => {console.log("error")});
+        },
+        methods: {
+            logTabs: function (tabs) {
+                for (let tab of tabs){
+                    console.log(tab);
+                }
+            }
+        }
+
+
+
+
+    }
+</script>
+
+<style scoped>
+
+</style>
